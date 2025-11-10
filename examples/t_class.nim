c16ce80415edca6146c8add740157b27388e7c37
Vladimir Berezenko <qmaster2000@gmail.com>
2025-11-10 21:06:00 +0300

    Added some example code

---
+++ examples/t_class.nim
@@ -0,0 +1,34 @@
+import ../klassifier
+
+class TClass:
+  var x: int
+  var x1*: int
+  var x2: int = 8
+  var x3*: int = 8
+  let y: float
+  let y1*: float
+  let y2: float = 1.0
+  let y3*: float = 1.0
+  method tMethod(self: TClass) = echo "TCLASS tmethod"
+  method tMethod1*(self: TClass) = echo "TCLASS tmethod1"
+  method tMethod2(self: TClass) = discard
+  proc tProc(self: TClass) = discard
+  proc tProc1*(self: TClass) = discard
+
+
+class TClass1 of TClass:
+  method tMethod(self: TClass1) = echo "TCLASS1 tmethod"
+  method tMethod3(self: TClass1) = discard
+  method `=destroy`*(self: TClass1) = echo "DESTRUCTION"
+  method `=new`*(self: TClass1) = echo "CONSTRUCTION"
+
+
+proc x(self: TClass1) =
+  TClass1.super tMethod(self)
+
+let cls = TClass1.new()
+
+cls.tMethod()
+TClass1.super tMethod(cls)
+TClass1.super cls.tMethod()
+cls.x()
